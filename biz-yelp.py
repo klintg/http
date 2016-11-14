825ea221445906323ced6e87ad60bd5e00c945e6
klintg <theklint1@gmail.com>
2016-11-14 15:40:24 +0300

    initial setup func

---
+++ biz-yelp.py
@@ -0,0 +1,19 @@
+# Working with the yelp api to get info on businesses. 
+import requests
+from requests_oauthlib import OAuth1          # https://github.com/requests/requests-oauthlib
+
+# yelp requires OAuth 
+consumerkey	= 'pnh2hlN4kBDkaf4TCCHX0Q'
+consumersecret ='hjng3Z0Q33vyvq999tvD0sUkVnY'
+token =	'K6RXGvphNoTZdkI-kyeXFEPYa6Uippck'
+tokensecret ='lh2UsKGzs0i-0vWhRVm4nF0aib8'
+
+def search_bizna(term, location):
+    url = 'https://api.yelp.com/v2/search'
+    args = {
+        "term" : term,
+        "location": location
+    }
+    authorize = OAuth1(consumerkey, consumersecret, token, tokensecret)
+    r = requests.get(url, auth=authorize, params=args)
+    return r 
